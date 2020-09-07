7c8bddd1f1aeb58569b359eaf820ad9ce5a3217a
lorenzopark <lorenzopark@postech.ac.kr>
2020-09-07 17:10:08 +0000

    fix gitignore

--- dann/utils/report.py
+++ dann/utils/report.py
@@ -8,9 +8,11 @@ class AverageMeter(object):
         self.avg = 0
         self.sum = 0
         self.count = 0
+        self.epoch = 0
 
     def update(self, val, n=1):
         self.val = val
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+        self.epoch += 1

90a8a07abd51ee814f33aa584e27898609575c63
lorenzopark <lorenzopark@postech.ac.kr>
2020-09-10 10:01:48 +0000

    fix dann

--- dann/utils/report.py
+++ dann/utils/report.py
@@ -2,17 +2,21 @@ class AverageMeter(object):
     """Computes and stores the average and current value"""
     def __init__(self):
         self.reset()
+        self.iterations = 0
+        
+    def __len__(self):
+        return self.iterations
 
     def reset(self):
         self.val = 0
         self.avg = 0
         self.sum = 0
         self.count = 0
-        self.epoch = 0
+        self.iterations = 0
 
     def update(self, val, n=1):
         self.val = val
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-        self.epoch += 1
+        self.iterations += 1
